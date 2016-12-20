45dc742d7b56bb25af7b95daa836c13e99332b05
Jason Eiben <jason.eiben@gmail.com>
2016-12-19 22:03:06 -0500

    Added partial templates to express_server.js

--- server_express/server_express.js
+++ server_express/server_express.js
@@ -7,15 +7,15 @@ app.set('view engine', 'ejs');
 
 //setting up the routes
 app.get('/', function(req,res){
-  res.sendFile(__dirname + '/index.html');
+  res.render('index');
 });
 
 app.get('/contact', function(req,res){
-  res.sendFile(__dirname + '/contact.html');
+  res.render('contact');
 });
 
 app.get('/profile/:name', function(req,res){
-  var data = {age: 29, job: 'ninja'};
+  var data = {age: 29, job: 'ninja', hobbies: ["eating", "fighting", "fishing"]};
   res.render('profile', {person: req.params.name, data: data});
 });
 
